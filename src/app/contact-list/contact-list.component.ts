065227a68f32cebe84c30bde2777602fefb48aa0
SerigneFallouNiang <fallouniang776@gmail.com>
2024-07-27 12:49:09 +0000

    authentification dans le platforme

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-contact-list',
@@ -8,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrl: './contact-list.component.css'
 })
 export class ContactListComponent {
+  loggedUser: any;
+  constructor(private router: Router) {
+    const localUser = localStorage.getItem('loggedUser');
+    if(localUser != null) {
+      this.loggedUser = JSON.parse(localUser);
+    }
+  }
 
+  onLogoff() {
+    localStorage.removeItem('loggedUser');
+    this.router.navigateByUrl('/login')
+  }
 }
