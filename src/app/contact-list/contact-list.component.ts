f3c736a0e4d9a031546a8f42e27d6a6892948095
loctetgenius <sagnamoussa546@gmail.com>
2024-07-29 00:12:59 +0000

    Ajout de la fonctionnalité de recherche de contact

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -2,17 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { Contact } from '../models/contact.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contacts-list',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css'],
 })
 export class ContactsListComponent implements OnInit {
   contacts: Contact[] = [];
   loggedUser: any;
+  searchQuery: string = '';
 
   constructor(private router: Router) { }
 
@@ -21,6 +23,21 @@ export class ContactsListComponent implements OnInit {
     const allContacts = JSON.parse(localStorage.getItem('contacts') || '[]');
     this.contacts = allContacts.filter(
       (contact: Contact) => contact.createdBy === this.loggedUser.email
+    );
+  }
+
+  filteredContacts(): Contact[] {
+    if (!this.searchQuery) {
+      return this.contacts;
+    }
+
+    const query = this.searchQuery.toLowerCase();
+
+    return this.contacts.filter(contact =>
+      contact.nom.toLowerCase().includes(query) ||
+      contact.prenom.toLowerCase().includes(query) ||
+      contact.email.toLowerCase().includes(query) ||
+      contact.telephone.includes(query)
     );
   }
 
